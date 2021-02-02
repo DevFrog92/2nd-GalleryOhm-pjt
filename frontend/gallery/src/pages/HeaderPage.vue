7371f2b80e091e128f088266898c09912989b255
권용수 <yskwon0619@gmail.com>
2021-02-02 09:50:15 +0900

    [0202] Update gallery render page

--- frontend/gallery/src/pages/HeaderPage.vue
+++ frontend/gallery/src/pages/HeaderPage.vue
@@ -10,9 +10,9 @@
       <div class="nav">
       <router-link class="nav-item" to="/">Main</router-link>
       <router-link class="nav-item" to="/listitem">List</router-link>
-      <!-- <router-link class="nav-item" to="/detail">Detail</router-link> -->
-      <!-- <router-link class="nav-item" to="/mypage">Mypage</router-link> -->
-      <!-- <router-link class="nav-item" to="/galleryrender">GalleryRender</router-link> -->
+      <router-link class="nav-item" to="/detail">Detail</router-link>
+      <router-link class="nav-item" to="/mypage">Mypage</router-link>
+      <router-link class="nav-item" to="/galleryrender">GalleryRender</router-link>
       <div v-if="loginState">
       <router-link class="nav-item" to="/workupload">WorkUpLoad</router-link>
       <router-link class="nav-item" to="/settings">Settings</router-link>
