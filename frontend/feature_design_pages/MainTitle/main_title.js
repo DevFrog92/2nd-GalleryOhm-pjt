1979df7e02ba3c21f0bea50d2f4514e08dc50058
강민지 <hlery1206@naver.com>
2021-02-05 02:40:56 +0900

    [0204] Create MainTitle

---
+++ frontend/feature_design_pages/MainTitle/main_title.js
@@ -0,0 +1,13 @@
+(function(){
+  
+
+var wrapper = document.getElementsByClassName("text-animation")[0];
+
+wrapper.innerHTML = wrapper.textContent.replace(/./g,"<span>$&</span>");
+
+var spans = wrapper.getElementsByTagName("span");
+
+for(var i=0;i<spans.length;i++){
+  spans[i].style.animationDelay = i*80+"ms";
+}
+})();
