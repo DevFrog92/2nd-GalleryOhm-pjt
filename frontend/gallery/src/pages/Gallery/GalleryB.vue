7bc2c13b1b8413c5022efee61acd9bb1bf5f206e
봉유정 <qhddbwjd@naver.com>
2021-02-08 21:12:26 +0900

    modify subgallery design

--- frontend/gallery/src/pages/Gallery/GalleryB.vue
+++ frontend/gallery/src/pages/Gallery/GalleryB.vue
@@ -4,11 +4,14 @@
     <div class="page">{{ nowPage }} / {{ imgList.length }}</div>
     <div v-for="(img, i) in imgList" :key="i">
       <div class="image" @mouseover="changePage(i + 1)">
-        <div class="content">
-          <!-- 필명 -->
-          <h1 class="text">{{ img.work_artistId }}</h1>
-        </div>
         <img :src="img.work_piece" :alt="img.work_title" />
+        <div class="content">
+          <!-- 작품명 -->
+          <h1 class="text">{{ img.work_title }}</h1>
+        </div>
+        <div class="bottom">
+          <!-- 작가, 올린날짜 -->
+        </div>
       </div>
     </div>
   </div>
@@ -52,9 +55,9 @@ export default {
 
 <style scoped>
 .subgallery {
-  height: 100vh;
+  /* height: 100vh; */
   overflow-x: hidden;
-  background-color: blanchedalmond;
+  background-color: #fcf1e7;
 }
 
 .galleryType {
@@ -71,12 +74,18 @@ export default {
   top: 45vh;
 }
 
+.galleryType,
+.page {
+  color: #175711;
+  font-family: "Hanna", sans-serif;
+}
+
 .subgallery::-webkit-scrollbar {
   display: none;
 }
 
-.images {
-  display: block;
+.image {
+  position: relative;
   width: auto;
   height: 100vh;
   z-index: 1;
@@ -84,15 +93,33 @@ export default {
 
 .image img {
   height: 80vh;
-  width: auto;
+  max-width: 90vw;
+  position: relative;
+  top: 10vh;
+
+  -webkit-transform: scale(1);
+  -moz-transform: scale(1);
+  -ms-transform: scale(1);
+  -o-transform: scale(1);
+  transform: scale(1);
+  -webkit-transition: 0.3s;
+  -moz-transition: 0.3s;
+  -ms-transition: 0.3s;
+  -o-transition: 0.3s;
+  transition: 0.9s;
+}
+
+.image img:hover {
+  -webkit-transform: scale(1.02);
+  -moz-transform: scale(1.02);
+  -ms-transform: scale(1.02);
+  -o-transform: scale(1.02);
+  transform: scale(1.02);
 }
 
 .image .content {
   position: relative;
-  top: 54%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  /* color: white; */
+  top: -35%;
   z-index: 2;
   text-align: center;
 }
@@ -100,14 +127,15 @@ export default {
 .image .content .text {
   -webkit-text-stroke: 3px rgb(46, 211, 31);
   -webkit-text-fill-color: transparent;
-  font-size: 10rem;
   font-family: "Hanna", sans-serif;
+  transform: scale(3);
+  opacity: 0;
 }
 
-img:hover .text {
-  -webkit-text-stroke: 3px rgb(46, 211, 31);
-  -webkit-text-fill-color: transparent;
-  font-size: 10rem;
-  font-family: "Hanna", sans-serif;
+.image img:hover ~ .content .text,
+.image .content:hover .text {
+  opacity: 1;
+  transform: scale(3.3);
+  transition: all 1.2s;
 }
 </style>
