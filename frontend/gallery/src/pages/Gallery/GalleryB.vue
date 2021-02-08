54ef3d999289bd2fcb8f61725157c3fb9ef9823c
봉유정 <qhddbwjd@naver.com>
2021-02-08 19:02:01 +0900

    modify galleryPage design, modify HeaderPage css

---
+++ frontend/gallery/src/pages/Gallery/GalleryB.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="subgallery" v-if="imgList.length != 0">
+    <p class="galleryType">서브갤러리</p>
+    <div class="page">{{ nowPage }} / {{ imgList.length }}</div>
+    <div v-for="(img, i) in imgList" :key="i">
+      <div class="image" @mouseover="changePage(i + 1)">
+        <div class="content">
+          <!-- 필명 -->
+          <h1 class="text">{{ img.work_artistId }}</h1>
+        </div>
+        <img :src="img.work_piece" :alt="img.work_title" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import http from "../../api/http.js";
+
+export default {
+  data() {
+    return {
+      imgList: [],
+      nowPage: 1,
+    };
+  },
+  components: {},
+  created() {
+    http.get(`/gallery/getAllSubGallery`).then(
+      (response) => {
+        var data = response.data;
+        for (var i = 0; i < data.length; i++) {
+          data[i].work_piece = "data:image/jpeg;base64," + data[i].work_piece;
+        }
+        this.imgList = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    this.nowPage = 1;
+  },
+  destroyed() {},
+  methods: {
+    changePage(i) {
+      this.nowPage = i;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.subgallery {
+  height: 100vh;
+  overflow-x: hidden;
+  background-color: blanchedalmond;
+}
+
+.galleryType {
+  position: fixed;
+  transform: rotate(-90deg);
+  left: 5vw;
+  top: 45vh;
+}
+
+.page {
+  position: fixed;
+  transform: rotate(90deg);
+  right: 5vw;
+  top: 45vh;
+}
+
+.subgallery::-webkit-scrollbar {
+  display: none;
+}
+
+.images {
+  display: block;
+  width: auto;
+  height: 100vh;
+  z-index: 1;
+}
+
+.image img {
+  height: 80vh;
+  width: auto;
+}
+
+.image .content {
+  position: relative;
+  top: 54%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  /* color: white; */
+  z-index: 2;
+  text-align: center;
+}
+
+.image .content .text {
+  -webkit-text-stroke: 3px rgb(46, 211, 31);
+  -webkit-text-fill-color: transparent;
+  font-size: 10rem;
+  font-family: "Hanna", sans-serif;
+}
+
+img:hover .text {
+  -webkit-text-stroke: 3px rgb(46, 211, 31);
+  -webkit-text-fill-color: transparent;
+  font-size: 10rem;
+  font-family: "Hanna", sans-serif;
+}
+</style>
