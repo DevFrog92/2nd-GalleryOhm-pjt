1bc9ceca8b952b84ffa507ef9b69b1ee9a68372d
정수빈 <wwaterbeen@gmail.com>
2021-02-09 15:17:50 +0900

    add resize test

--- frontend/gallery/src/pages/Gallery/GalleryTest.vue
+++ frontend/gallery/src/pages/Gallery/GalleryTest.vue
@@ -1,29 +1,64 @@
 <template>
-<div>      
-<div class="btn">
-  <router-link to="/test/GalleryO"><b-button variant="success">메인 갤러리</b-button></router-link>
-</div>
+  <div>
+    <div class="btn">
+      <router-link to="/test/GalleryO"
+        ><b-button variant="success">메인 갤러리</b-button></router-link
+      >
+    </div>
 
-  <router-link to="/test/GalleryB"><b-button class="b3" variant="warning">서브 갤러리</b-button></router-link>
-  <router-link to="/test/GalleryJ"><b-button class="b2" variant="danger">레드 갤러리</b-button></router-link>
-</div>
-    
+    <router-link to="/test/GalleryB"
+      ><b-button class="b3" variant="warning"
+        >서브 갤러리</b-button
+      ></router-link
+    >
+    <router-link to="/test/GalleryJ"
+      ><b-button class="b2" variant="danger">레드 갤러리</b-button></router-link
+    >
+
+    <div class="btn2">
+      <router-link to="/test/resize1"
+        ><b-button>리사이징 1</b-button></router-link
+      >
+    </div>
+    <router-link to="/test/resize2"
+      ><b-button class="b4">리사이징 2</b-button></router-link
+    >
+    <router-link to="/test/resize3"
+      ><b-button class="b5">리사이징 3</b-button></router-link
+    >
+  </div>
 </template>
 
 <style scoped>
+.btn2 {
+  width: 10px;
+  height: 10px;
+  position: absolute;
+  left: 50%;
+  top: 10%;
+  margin-left: -200px;
+}
 .btn {
-    width:300px;
-    height:300px;
-    position:absolute;
-    left:50%;
-    top:50%;
-    margin-left:-350px;
-    margin-top:-150px;
+  width: 300px;
+  height: 300px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  margin-left: -350px;
+  margin-top: -150px;
 }
 .b2 {
-    margin-left: 1%;
+  margin-left: 1%;
 }
 .b3 {
-    margin-left: 30%;
+  margin-left: 30%;
+}
+.b4 {
+  margin-left: 1%;
+  top: 10%;
+}
+.b5 {
+  margin-left: 30%;
+  top: 10%;
 }
 </style>
---
+++ frontend/gallery/src/pages/Gallery/resize1.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <input type="file" @change="onFileChange" />
+
+    <div id="preview">
+      <img v-if="url" :src="url" />
+    </div>
+
+    <button>업로드</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      url: null,
+    };
+  },
+  methods: {
+    onFileChange(e) {
+      const file = e.target.files[0];
+      this.url = URL.createObjectURL(file);
+    },
+  },
+};
+</script>
+
+<style scoped>
+body {
+  background-color: #e2e2e2;
+}
+
+#app {
+  padding: 20px;
+}
+
+#preview {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#preview img {
+  max-width: 100%;
+  max-height: 500px;
+}
+</style>
