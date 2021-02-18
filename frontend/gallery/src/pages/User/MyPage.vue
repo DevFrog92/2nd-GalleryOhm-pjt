d7d29700cf1cb2210c92a068a13489f0fd476416
권용수 <yskwon0619@gmail.com>
2021-02-19 05:58:26 +0900

    [0219] Update front end

--- frontend/gallery/src/pages/User/MyPage.vue
+++ frontend/gallery/src/pages/User/MyPage.vue
@@ -166,8 +166,8 @@
           <img v-if="my_gallery_1.gallery_id" :src="'data:/image/jpeg;base64,' + my_gallery_poster[0] " alt="">
 
           <h3>{{my_gallery_1.gallery_name}}</h3>
-          <p v-if="my_gallery_1.gallery_id">작가 : {{my_gallery_1.gallery_artistId}}
-            {{my_gallery_1.gallery_writeTime.slice(0,10)}} ~</p>
+          <div v-if="my_gallery_1.gallery_id" class="poster__aside__id">{{my_gallery_1.gallery_artistId}}</div>
+          <div v-if="my_gallery_1.gallery_id" class="poster__aside__date">  {{my_gallery_1.gallery_writeTime.slice(0,10)}} ~</div>
         </div>
 
         <div class="Mypage__poster_card2" :data-value="my_gallery_2.gallery_id">
@@ -187,8 +187,9 @@
           </div>
           <img v-if="my_gallery_2.gallery_id" :src="'data:/image/jpeg;base64,' + my_gallery_poster[1] " alt="">
           <h3>{{my_gallery_2.gallery_name}}</h3>
-          <p v-if="my_gallery_2.gallery_id">Created By {{my_gallery_2.gallery_artistId}}
-            {{my_gallery_2.gallery_writeTime.slice(0,10)}} ~</p>
+
+             <div v-if="my_gallery_2.gallery_id" class="poster__aside__id">{{my_gallery_2.gallery_artistId}}</div>
+           <div v-if="my_gallery_2.gallery_id" class="poster__aside__date">  {{my_gallery_2.gallery_writeTime.slice(0,10)}} ~</div>
         </div>
       </div>
 
